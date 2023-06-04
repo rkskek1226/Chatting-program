ee80543aeb522cc5227869f3baa4684968496c62
rkskek1226 <hkcho01@naver.com>
2023-06-04 17:35:31 +0900

    header

---
+++ pipe_header.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <pthread.h>
+
+#define BUF_SIZE 30
+
+
